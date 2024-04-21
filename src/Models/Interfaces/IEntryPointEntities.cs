dc5ed47471cd46bd77e4c541813f0f44fbf89556
Bodden007 <denis-bodrov@mail.ru>
2024-04-21 20:12:52 +0500

    Added Entities

---
+++ src/Models/Interfaces/IEntryPointEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRS.src.Models.Interfaces
+{
+    internal interface IEntryPointEntities
+    {
+        const int RSI_High = 70;
+        const int RSI_Low = 30;
+    }
+}
---
+++ src/Models/Interfaces/IUserEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRS.src.Models.Interfaces
+{
+    
+    internal interface IUserEntities
+    {
+        decimal Money { get; set; }
+        string StocksTicker { get; set; }
+        int QuantityStocks { get; set; }
+    }
+}
