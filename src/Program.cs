9fa15561feaacd18a1a7b30c3f394a4d7b21c40d
Bodden007 <denis-bodrov@mail.ru>
2024-04-28 16:59:54 +0500

    Installation the Logger

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,22 @@
-﻿using TRS.src.Models.Entities;
+﻿using Serilog;
+using TRS.src.Models.Entities;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        //FIXME Logger Serilog seated to the console, need set out to file!!!!
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss.fff zzz} " +
+           "[{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
+
+        var log=Log.ForContext<Program>();
+
+        log.Information("Main");
+
+
         //TODO Точка Входа
     }
 }
